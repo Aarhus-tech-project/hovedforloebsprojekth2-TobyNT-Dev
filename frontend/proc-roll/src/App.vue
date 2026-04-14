d47189af93197a8c2932cd60b7256652cc2843f7
Tobias Thy <t.nt.dev.code@gmail.com>
2026-04-14 14:06:02 +0200

    Initial commit, frontend, backend

---
+++ frontend/proc-roll/src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import GameContainer from './components/GameContainer.vue';
+</script>
+
+<template>
+  <h1>Proc Roll</h1>
+  <GameContainer />
+</template>
---
+++ frontend/proc-roll/src/components/GameContainer.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <div class="game-container">
+      <h2>Game Container</h2>
+    </div>
+    
+</template>
+
+
+
+
+<script>
+console.warn("GameContainer.vue loaded");
+
+export default {name: 'GameContainer'};
+</script>
